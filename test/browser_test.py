fa96a8c5559d8c33e4bf4913043e8c5dd4c46758
Kevin Bell <kevin.a.bell@gmail.com>
2015-11-16 20:05:48 -0800

    Use js

--- test/browser_test.py
+++ test/browser_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.alert import Alert
 
 
 driver = webdriver.Remote(
@@ -7,6 +8,7 @@ driver = webdriver.Remote(
     command_executor="http://localhost:4444"
 )
 driver.get('http://nginx:8000')
-print driver.page_source
-assert 'Hello' in driver.page_source
+alert_text = Alert(driver).text
+print alert_text
+assert 'Hello' in alert_text
 driver.quit()
--- uwsgi/hello.py
+++ uwsgi/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return '<script>alert("Hello World!")</script>'
 
 if __name__ == "__main__":
     app.run()
