edeb6ae12f1487dc16dfcab6e48f26f42179cea6
Kevin Bell <kevin.a.bell@gmail.com>
2015-12-03 12:41:51 -0800

    Make test

--- test/browser_test.py
+++ test/browser_test.py
@@ -1,16 +1,21 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.alert import Alert
 
+host = os.environ['DOCKER_IP']
 
-driver = webdriver.Remote(
-    desired_capabilities=DesiredCapabilities.CHROME,
-    command_executor="http://localhost:4444"
-)
-driver.get('http://nginx:8000')
-alert = Alert(driver)
-print alert.text
-assert 'Hello' in alert.text
-alert.accept()
-driver.get_screenshot_as_file('screenshot.png')
-driver.quit()
+
+def test_alert():
+    driver = webdriver.Remote(
+        desired_capabilities=DesiredCapabilities.CHROME,
+        command_executor="http://%s:4444" % host
+    )
+    driver.get('http://nginx:8000')
+    alert = Alert(driver)
+    print alert.text
+    assert 'Hello' in alert.text
+    alert.accept()
+    driver.get_screenshot_as_file('screenshot.png')
+    driver.quit()
